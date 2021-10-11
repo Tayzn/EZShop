18938be0c3e3ae254425b81c04aa10e1e81fbe87
Austin Cory Bart <acbart@vt.edu>
2021-10-11 16:31:05 -0400

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -7,17 +7,10 @@ function App(): JSX.Element {
         <div className="App">
             <header className="App-header">
                 <img src={logo} className="App-logo" alt="logo" />
+                UD CIS Scheduler
                 <p>
-          Edit <code>src/App.tsx</code> and save to reload.
+                    Edit <code>src/App.tsx</code> and save to reload.
                 </p>
-                <a
-                    className="App-link"
-                    href="https://reactjs.org"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                >
-          Learn React
-                </a>
             </header>
         </div>
     );
