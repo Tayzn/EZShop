0736d09fc03f1825e858d811f92b6e4776f99bbb
Austin Cory Bart <acbart@vt.edu>
2022-01-29 13:15:20 -0500

    Modify text slightly of App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -8,7 +8,8 @@ function App(): JSX.Element {
                 UD CISC275 with React Hooks and TypeScript
             </header>
             <p>
-                Edit <code>src/App.tsx</code> and save to reload.
+                Edit <code>src/App.tsx</code> and save. This page will
+                automatically reload.
             </p>
         </div>
     );
