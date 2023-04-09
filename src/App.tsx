4d88303bdb0d7178fdf0b86fe9a8343e03aff0c4
Matthew Meredith <merematt@udel.edu>
2023-04-09 02:00:03 -0400

    super janky terrible temporary data viewer/editor

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import "./App.css";
 import { Filter } from "./components/Filter";
 import { AuthComponent } from "./components/AuthComponent";
+import { TempDataComponent } from "./components/TempDataComponent";
 function App(): JSX.Element {
     return (
         <div className="App">
@@ -14,6 +15,7 @@ function App(): JSX.Element {
             <p>Paul Kearney</p>
             {<Filter></Filter>}
             <p>Matt Meredith</p>
+            <TempDataComponent />
             <p>Nick Lago</p>
             <p>Evan Lewis</p>
             <p>Kay Sousa</p>
