30e5bb0a3c72282c2120c3ea6a674d0ea40570c9
Nick <you@example.com>
2023-04-11 16:57:20 -0400

    second banner

--- src/components/ImageBanner.tsx
+++ src/components/ImageBanner.tsx
@@ -1,12 +1,38 @@
-import React from "react";
+import React, { useState } from "react";
 import { Container, Image } from "react-bootstrap";
 
-const bannerUrl = "https://i.ibb.co/BKgX1tt/bannergrad.png";
-
 export const ImageBanner = (): JSX.Element => {
+    const imgList = [
+        "https://i.ibb.co/BKgX1tt/bannergrad.png",
+        "https://i.ibb.co/XLNVbHW/secondbanner.png"
+    ];
+
+    const [bannerUrl, setUrl] = useState<string>(
+        "https://i.ibb.co/BKgX1tt/bannergrad.png"
+    );
+    const [imgIndex, setIndex] = useState<number>(0);
+
+    function changeImage() {
+        if (imgIndex == 0) {
+            setUrl(imgList[1]);
+            setIndex(1);
+        } else {
+            setUrl(imgList[0]);
+            setIndex(0);
+        }
+    }
+
+    setInterval(() => {
+        changeImage();
+    }, 7 * 1000);
+
+    const bannerStyle = {
+        transition: "opacity 0.5s ease-in-out"
+    };
+
     return (
         <Container fluid className="p-0 m-0">
-            <Image fluid src={bannerUrl} />
+            <Image style={bannerStyle} fluid src={bannerUrl} />
         </Container>
     );
 };
