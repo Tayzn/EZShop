add1a57bd066884e4750bff86201babd28e29e6d
Nick <you@example.com>
2023-04-11 22:20:07 -0400

    added banners

--- src/components/ImageBanner.tsx
+++ src/components/ImageBanner.tsx
@@ -1,10 +1,12 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { Container, Image } from "react-bootstrap";
 
 export const ImageBanner = (): JSX.Element => {
     const imgList = [
         "https://i.ibb.co/BKgX1tt/bannergrad.png",
-        "https://i.ibb.co/XLNVbHW/secondbanner.png"
+        "https://i.ibb.co/XLNVbHW/secondbanner.png",
+        "https://i.ibb.co/kgLCHp3/thirdbanner.png",
+        "https://i.ibb.co/wRfrX4R/fourthbanner.png"
     ];
 
     const [bannerUrl, setUrl] = useState<string>(
@@ -13,26 +15,42 @@ export const ImageBanner = (): JSX.Element => {
     const [imgIndex, setIndex] = useState<number>(0);
 
     function changeImage() {
-        if (imgIndex == 0) {
-            setUrl(imgList[1]);
-            setIndex(1);
-        } else {
+        if (imgIndex == imgList.length - 1) {
             setUrl(imgList[0]);
             setIndex(0);
+        } else {
+            setUrl(imgList[imgIndex + 1]);
+            setIndex(imgIndex + 1);
         }
     }
 
-    setInterval(() => {
-        changeImage();
-    }, 7 * 1000);
+    useEffect(() => {
+        const interval = setInterval(() => {
+            changeImage();
+        }, 2000);
+
+        return () => clearInterval(interval);
+    });
 
     const bannerStyle = {
-        transition: "opacity 0.5s ease-in-out"
+        top: "1000px",
+        transition: "opacity 0.5s ease-in-out",
+        opacity: "1"
+    };
+
+    const fadedBannerStyle = {
+        top: "300px",
+        transition: "opacity 0.5s ease-in-out",
+        opacity: "1"
     };
 
     return (
         <Container fluid className="p-0 m-0">
-            <Image style={bannerStyle} fluid src={bannerUrl} />
+            <Image
+                style={imgIndex === 0 ? bannerStyle : fadedBannerStyle}
+                fluid
+                src={bannerUrl}
+            />
         </Container>
     );
 };
