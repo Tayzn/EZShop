20b37cae140457c31d226a73f3fd25c84c5c9b20
Tayzn <tyguy2464@gmail.com>
2023-04-16 15:44:13 -0400

    Finish admin setup files

---
+++ src/components/admin/AdminInventory.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+import { Container } from "react-bootstrap";
+
+export const AdminInventory = (): JSX.Element => {
+    return (
+        <Container>
+            <h2>Inventory</h2>
+        </Container>
+    );
+};
---
+++ src/components/admin/AdminOrders.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+import { Container } from "react-bootstrap";
+
+export const AdminOrders = (): JSX.Element => {
+    return (
+        <Container>
+            <h2>Orders</h2>
+        </Container>
+    );
+};
