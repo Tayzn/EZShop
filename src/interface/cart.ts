6410a9fe78f8900692eff656287c1767e6d1a595
Matthew Meredith <merematt@udel.edu>
2023-04-20 12:12:10 -0400

    updated the interfaces

--- src/interface/cart.ts
+++ src/interface/cart.ts
@@ -4,8 +4,9 @@
  * 4/8/2023
  */
 
+import { auth_HookUser } from "../firebase/firebase_auth";
+import { CartData } from "../firebase/firebase_data";
 import { Product, ProductVariant } from "./product";
-import { UserId } from "./account";
 
 /**
  * Shared attributes for both the local and database versions of CartItem
@@ -26,6 +27,52 @@ export interface CartItem {
  * A users Cart
  */
 export interface Cart {
-    owner: UserId;
     items: CartItem[];
+}
+
+let cartOwner: string | null;
+let cart: Cart;
+
+export function initializeCart() {
+    auth_HookUser((user) => {
+        if (user) {
+            CartData.get(user.uid).then((newCart) => {
+                cart = newCart.data;
+                cartOwner = newCart.reference.id;
+            });
+            // TODO merge/clear localstorage
+        } else {
+            cart = { items: [] };
+            cartOwner = null;
+            // TODO load localstorage
+        }
+    });
+}
+
+/**
+ * Get the current cart
+ *
+ * This function will transparently select between a logged in users cart and a local cart
+ */
+export function getCart(): Cart {
+    return cart;
+}
+
+/**
+ * Save the current cart
+ *
+ * This function will transparently save to either the database for a logged in user, or local storage
+ */
+export function saveCart(): Promise<void> {
+    return new Promise((resolve, reject) => {
+        if (cartOwner) {
+            // save to database
+            CartData.update(cartOwner, cart)
+                .then(() => resolve())
+                .catch(reject);
+        } else {
+            // todo save localstorage
+            resolve();
+        }
+    });
 }
