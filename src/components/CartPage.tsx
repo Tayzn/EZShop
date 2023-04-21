0c3173abc2515db175618f48230d87ec3058a4da
Matthew Meredith <merematt@udel.edu>
2023-04-21 11:30:26 -0400

    fixed cart not loading because it wasn't a state

--- src/components/CartPage.tsx
+++ src/components/CartPage.tsx
@@ -1,19 +1,47 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
-import { Container } from "react-bootstrap";
-import { getCart } from "../interface/cart";
+import { Button, Container } from "react-bootstrap";
+import {
+    Cart,
+    addToCart,
+    cart_HookCartState,
+    getCart
+} from "../interface/cart";
+import { ProductData } from "../firebase/firebase_data";
 
 export const CartPage = (): JSX.Element => {
+    const [cart, setCart] = useState<Cart>(getCart());
+    useEffect(() => cart_HookCartState(setCart), []);
     return (
         <Container fluid className="flex-grow-1 ez-bg">
             <Container className="h-100 side-shadow">
                 <h1 className="p-4">Your Cart</h1>
                 <hr></hr>
-                {getCart()?.items.map((item, idx) => (
+                {cart.items.map((item, idx) => (
                     <p key={idx}>
                         {item.product.name} - x{item.quantity}
                     </p>
                 ))}
+                <Button
+                    onClick={() => {
+                        ProductData.list().then((items) => {
+                            const item =
+                                items[Math.floor(Math.random() * items.length)];
+                            addToCart(
+                                item.data,
+                                Math.floor(Math.random() * items.length),
+                                item.data.variants
+                                    ? Math.floor(
+                                          Math.random() *
+                                              item.data.variants.length
+                                      )
+                                    : null
+                            );
+                        });
+                    }}
+                >
+                    Add Debug Item
+                </Button>
             </Container>
         </Container>
     );
