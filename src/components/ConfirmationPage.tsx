0f0a9ec9ed9e525221e346c96ece7554a393f9d1
printemps25 <124960699+printemps25@users.noreply.github.com>
2023-05-01 15:52:20 -0400

    Update ConfirmationPage.tsx

--- src/components/ConfirmationPage.tsx
+++ src/components/ConfirmationPage.tsx
@@ -1,30 +1,42 @@
 import React, { useEffect, useState } from "react";
 import { Container, Button, Col } from "react-bootstrap";
 import { CartItemDisplay } from "./cart/CartItemDisplay";
-import { Cart, cart_HookCartState, getCart } from "../interface/cart";
+import { useLocation } from "react-router-dom";
+import { Cart, CartItem, cart_HookCartState, getCart } from "../interface/cart";
 
 export const ConfirmationPage = (): JSX.Element => {
     const [cart, setCart] = useState<Cart>(getCart());
     useEffect(() => cart_HookCartState(setCart), []);
+    const location = useLocation();
+    const cartItems = location.state?.cartItems || [];
 
     return (
         <Container fluid style={{ height: "90%" }}>
-            <h1 className="confirm">Order Placed!</h1>
-            <h2 className="items">items here</h2>
-
+            <h1
+                style={{
+                    fontFamily: "Arial",
+                    fontSize: "3rem",
+                    marginBottom: "2rem"
+                }}
+            >
+                Order Placed!
+            </h1>
+            <Col xs={8}>
+                <h4>Items in Cart</h4>
+                {cartItems.map((item: CartItem, idx: number) => (
+                    <CartItemDisplay key={idx} item={item} />
+                ))}
+            </Col>
             <Button
                 variant="success"
                 type="submit"
                 className="confirmButton"
                 href="#/"
+                style={{ marginTop: "2rem" }}
             >
                 Return Home
             </Button>
-            <Col xs={8}>
-                {cart.items.map((item, idx) => (
-                    <CartItemDisplay key={idx} item={item} />
-                ))}
-            </Col>
         </Container>
     );
 };
+
