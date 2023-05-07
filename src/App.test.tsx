c539b04661519af38caba78dabd8b20cc2edfe51
Tayzn <tyguy2464@gmail.com>
2023-05-06 21:33:56 -0400

    Random test stuff

--- src/App.test.tsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import { render, screen } from "@testing-library/react";
-import App from "./App";
-
-test("renders the course name somewhere", () => {
-    render(<App />);
-    const linkElement = screen.getByText(/CISC275/i);
-    expect(linkElement).toBeInTheDocument();
-});
