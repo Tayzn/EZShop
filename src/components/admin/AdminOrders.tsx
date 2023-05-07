7be331ca47b748d8c3e8412a3ff68b906b426801
evan <evan@lewiscenter.net>
2023-05-07 19:43:36 -0400

    Order table basics implemented

--- src/components/admin/AdminOrders.tsx
+++ src/components/admin/AdminOrders.tsx
@@ -1,9 +1,9 @@
 import React, { useState } from "react";
 
-import { Container, Table, Alert } from "react-bootstrap";
-import { useAllOrders } from "../../interface/order";
-import { useLoggedInUser } from "../../firebase/firebase_auth";
 import { User } from "firebase/auth";
+import { Alert, Button, Container, Table } from "react-bootstrap";
+import { useLoggedInUser } from "../../firebase/firebase_auth";
+import { setOrderStatus, useAllOrders } from "../../interface/order";
 
 export const AdminOrders = (): JSX.Element => {
     const [loaded, setLoaded] = useState<boolean>(false);
@@ -41,21 +41,92 @@ export const AdminOrders = (): JSX.Element => {
                 <Table striped className="mt-2">
                     <thead>
                         <tr>
-                            <th>Product Name</th>
-                            <th>Price</th>
-                            <th>Stock</th>
-                            <th>Modify</th>
+                            <th>Order ID</th>
+                            <th>Order Status</th>
+                            <th>User</th>
+                            <th>Total</th>
+                            <th>Cart</th>
                         </tr>
                     </thead>
                     <tbody>
-                        {orders.map((order) => {
+                        {orders.map((order, index) => {
+                            let total = 0;
+
+                            order.data.items.map((item) => {
+                                {
+                                    total += item.product.price * item.quantity;
+                                }
+                            });
+
                             return (
-                                <tr key={order.reference.id}>
-                                    <td>{order.data.user}</td>
+                                <tr key={index}>
+                                    <td>#{index}</td>
                                     <td>{order.data.status}</td>
-                                    <td>{order.data.items[0].product.name}</td>
-                                    <td>{order.data.address.addr1}</td>
                                     <td>{order.data.payment.cardholderName}</td>
+                                    <td>${total}</td>
+                                    <td>
+                                        <Button
+                                            size="sm"
+                                            variant="info"
+                                            onClick={() => {
+                                                console.log("click");
+                                            }}
+                                        >
+                                            View
+                                        </Button>
+                                    </td>
+                                    <td>
+                                        {order.data.status == "pending" ? (
+                                            <div>
+                                                <Button
+                                                    size="sm"
+                                                    variant="success"
+                                                    className="me-2"
+                                                    onClick={() => {
+                                                        setOrderStatus(
+                                                            order,
+                                                            "complete"
+                                                        );
+                                                    }}
+                                                >
+                                                    Approve
+                                                </Button>
+                                                <Button
+                                                    variant="danger"
+                                                    size="sm"
+                                                    onClick={() => {
+                                                        setOrderStatus(
+                                                            order,
+                                                            "cancelled"
+                                                        );
+                                                    }}
+                                                >
+                                                    Refund
+                                                </Button>
+                                            </div>
+                                        ) : (
+                                            <div className="d-grid gap-2">
+                                                {order.data.status ==
+                                                "complete" ? (
+                                                    <Button
+                                                        variant="success"
+                                                        size="sm"
+                                                        disabled
+                                                    >
+                                                        Approved
+                                                    </Button>
+                                                ) : (
+                                                    <Button
+                                                        variant="danger"
+                                                        size="sm"
+                                                        disabled
+                                                    >
+                                                        Refunded
+                                                    </Button>
+                                                )}
+                                            </div>
+                                        )}
+                                    </td>
                                 </tr>
                             );
                         })}
