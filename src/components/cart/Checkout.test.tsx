ae11aac7310daddd1b391181f5f4efdd75923ae0
Matthew Meredith <merematt@udel.edu>
2023-05-14 18:37:42 -0400

    fixed test run or something

--- src/components/cart/Checkout.test.tsx
+++ src/components/cart/Checkout.test.tsx
@@ -6,12 +6,49 @@ import { act } from "react-dom/test-utils";
 import { ConfirmationPage } from "../ConfirmationPage";
 import { PaymentModal } from "./PaymentModal";
 import { getDefaultPayment } from "../../interface/account";
+import "@testing-library/jest-dom";
+import { BrowserRouter } from "react-router-dom";
+import App from "../../App";
 
 const mockedUsedLocation = jest.fn();
+
 jest.mock("react-router-dom", () => ({
     ...jest.requireActual("react-router-dom"),
     useLocation: () => mockedUsedLocation
 }));
+jest.mock("../../interface/order.ts", () => ({
+    //...jest.requireActual("../../interface/order.ts"),
+    __esModule: true,
+    useOrder: () => ({
+        data: {
+            date: new Date(),
+            items: [],
+            user: "anonymous",
+            status: "pending",
+            address: {
+                addr1: "123 st st",
+                addr2: null,
+                city: "newark",
+                state: "de",
+                zip: "19713"
+            },
+            payment: {
+                cardholderName: "test cardholder",
+                cardNumber: "1234567887654321",
+                cvv: "123",
+                expiration: new Date(),
+                zip: "19713"
+            }
+        },
+        reference: null
+    })
+}));
+
+const renderWithRouter = (ui: JSX.Element, { route = "/" } = {}) => {
+    window.history.pushState({}, "Test page", route);
+
+    return render(ui, { wrapper: BrowserRouter });
+};
 
 initializeFirebase(false);
 
@@ -24,11 +61,16 @@ describe("Checkout Tests", () => {
 
     it("Confirmation page displays correct text and buttons", async () => {
         await act(async () => {
-            render(<ConfirmationPage />);
+            renderWithRouter(<App />, {
+                route: "/confirmation/dummy"
+            });
         });
 
+        await new Promise<void>((r) => setTimeout(r, 1000));
         const receiptText = screen.getByText(/receipt/i);
-        const homeButton = screen.getByRole("button", { name: /return home/i });
+        const homeButton = screen.getByRole("button", {
+            name: /return home/i
+        });
 
         expect(receiptText).toBeInTheDocument();
         expect(homeButton).toBeInTheDocument();
