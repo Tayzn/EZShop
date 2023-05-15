e6d74b755488cc37af4e1927989aab23b2c83c1f
printemps25 <124960699+printemps25@users.noreply.github.com>
2023-05-15 15:35:49 -0400

    Update ConfirmationPage.tsx

--- src/components/ConfirmationPage.tsx
+++ src/components/ConfirmationPage.tsx
@@ -1,10 +1,23 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { Container, Button, Alert } from "react-bootstrap";
 import { useParams } from "react-router-dom";
 import { BasicDisplayCard } from "../components/cart/BasicDisplayCard";
 import { CartItem } from "../interface/cart";
 import { Order, useOrder } from "../interface/order";
 import { ReferencedObject } from "../firebase/firebase_data";
+
+const generateTrackingNumber = () => {
+    const alphanumericCharacters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+    const trackingNumberLength = 14;
+    let trackingNumber = "";
+    for (let i = 0; i < trackingNumberLength; i++) {
+        const randomIndex = Math.floor(
+            Math.random() * alphanumericCharacters.length
+        );
+        trackingNumber += alphanumericCharacters[randomIndex];
+    }
+    return trackingNumber;
+};
 
 export const ConfirmationPage = (): JSX.Element => {
     const { orderID } = useParams();
@@ -19,6 +32,12 @@ export const ConfirmationPage = (): JSX.Element => {
               (reason) => setError(reason)
           )
         : undefined;
+
+    const [trackingNumber, setTrackingNumber] = useState<string>("");
+
+    useEffect(() => {
+        setTrackingNumber(generateTrackingNumber());
+    }, []);
 
     return order ? (
         <Container
@@ -42,6 +61,12 @@ export const ConfirmationPage = (): JSX.Element => {
                     <BasicDisplayCard key={idx} item={item} />
                 ))}
             </Container>
+            <p
+                className="text-center text-muted mt-3"
+                style={{ fontSize: "smaller" }}
+            >
+                <strong>Tracking Number:</strong> {trackingNumber}
+            </p>
             <Button variant="success" size="lg" href="#/" className="mt-4">
                 Return Home
             </Button>
