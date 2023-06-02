ceb8a37595769742acfb84e98d905a08614d5c9b
Tayzn <tyguy2464@gmail.com>
2023-06-02 15:46:59 -0400

    Cleanup

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -11,22 +11,12 @@ export const Footer = (): JSX.Element => {
             <div style={{ paddingLeft: "10px" }}>EZShop™</div>
 
             <Navbar.Collapse className="justify-content-end">
-                {window.innerWidth > 850 ? (
-                    <a
-                        href="https://github.com/Tayzn/EZShop/"
-                        style={{ paddingRight: "10px" }}
-                    >
-                        Credits : Tyler Nauta, Paul
-                        Kearney, Matt Meredith, Nick Lago, Evan Lewis, Kay Sousa
-                    </a>
-                ) : (
-                    <a
-                        href="https://github.com/Tayzn/EZShop/"
-                        style={{ paddingRight: "10px" }}
-                    >
-                        A simple web store
-                    </a>
-                )}
+                <a
+                    href="https://github.com/Tayzn/EZShop/"
+                    style={{ paddingRight: "10px" }}
+                >
+                    Source Code
+                </a>
             </Navbar.Collapse>
         </Navbar>
     );
